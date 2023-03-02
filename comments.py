6741ae29d20e620f0c5176c4d161ea828eeb50bc
Deepansubramani <deepansubramani20@gmail.com>
2023-03-02 11:59:05 +0530

    Deleted C Argument

--- comments.py
+++ comments.py
@@ -6,7 +6,7 @@ print ("Hello, World!")
 
 ##Doc_String Comments
 
-def add(a, b, c):
-    """"Function to add the value a,b,c"""
-    return a+b+c
+def add(a, b,):
+    """"Function to add the value a,b"""
+    return a+b
     print(add._doc_)
