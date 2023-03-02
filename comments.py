72856e9871529cd20e30a9bcfc73f8aa6326d96c
Deepansubramani <deepansubramani20@gmail.com>
2023-03-02 12:24:37 +0530

    Change the values

--- comments.py
+++ comments.py
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
 ## This is a single line comment in python
 print ("Hello, World!")
 
-
-
-
 ##Doc_String Comments
 
-def add(a, b, c):
-    """"Function to add the value a,b,c"""
-    return a+b+c
-    print(add._doc_)
-# This is a single line comment in python
-print ("Hello, World!")
-...
-  This is a multiline
-  comments.
- ...
+def add(a, b):
+    """Function to add the value a,b"""
+return a+b
+print(add._doc_)
+  ##This is a multiline comments.
 print("Hello Multiline Comments!")
