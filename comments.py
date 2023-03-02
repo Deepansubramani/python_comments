2e7947930cbecbe8fcc89baabd9037bb1dff69e0
Deepansubramani <deepansubramani20@gmail.com>
2023-03-02 11:34:45 +0530

    Doc_String

--- comments.py
+++ comments.py
@@ -1,2 +1,12 @@
-# This is a single line comment in python
+## This is a single line comment in python
 print ("Hello, World!")
+
+
+
+
+##Doc_String Comments
+
+def add(a, b):
+    """"Function to add the value a,b"""
+    return a+b
+    print(add._doc_)
